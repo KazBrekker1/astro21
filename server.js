bae2cced9bdb3cad81531edc76a5f4e2e59e1a44
KazBrekker1 <justanothernerd000@gmail.com>
2021-05-09 05:54:14 +0300

    - Added server File for Deployment T1

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const serveStatic = require("serve-static")
+const history = require("connect-history-api-fallback")
+const enforce = require("express-sslify")
+
+const app = express()
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }))
+
+app.get(["/login", "/register"], (req, res) => {
+	res.redirect("/")
+})
+
+app.use(serveStatic(__dirname + "/dist"))
+app.use(history())
+
+app.listen(process.env.PORT || 5000)
