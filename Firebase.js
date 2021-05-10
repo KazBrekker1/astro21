4609f17392dba5ac29ca9ff7dbc38d72f897af69
KazBrekker1 <justanothernerd000@gmail.com>
2021-05-10 04:24:36 +0300

    - Reverting Change...FB

--- Firebase.js
+++ Firebase.js
@@ -1,15 +1,11 @@
 import firebase from "firebase"
 
-console.log(process.env.VUE_APP_apiKey)
 const firebaseConfig = {
-	// apiKey: "AIzaSyBYAJH86SCAG4K8FtfwrPyjguNEXapBSMA",
 	apiKey: process.env.VUE_APP_apiKey,
 	authDomain: "astro21-a90b6.firebaseapp.com",
 	projectId: "astro21-a90b6",
 	storageBucket: "astro21-a90b6.appspot.com",
-	// messagingSenderId: "226656084400",
 	messagingSenderId: process.env.VUE_APP_messagingSenderId,
-	// appId: "1:226656084400:web:2b2020d2ec3d8517827675",
 	appId: process.env.VUE_APP_appId,
 }
 
