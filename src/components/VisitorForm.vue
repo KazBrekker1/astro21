a53fe845cb28fba7ed92db814b3778a8dec5b364
KazBrekker1 <justanothernerd000@gmail.com>
2021-10-08 15:58:25 +0300

    - Added Adding Workshops to the Visitors

--- src/components/VisitorForm.vue
+++ src/components/VisitorForm.vue
@@ -29,7 +29,7 @@
 				/>
 			</li>
 			<li class="list-group-item list-group-item-dark p-1 pb-0 m-1">
-				<select class="bg-transparent border-0 w-100 h5" v-model="p1D1" required>
+				<select class="bg-transparent border-0 w-100 h5" v-model="p1D1">
 					<option value disabled>Select p1D1</option>
 					<option
 						value="Eng. Naief Osman - Secrets and Mysteries of the Universe from the Atom to The Galaxy (Part I )|| أسرار الكون من الذرة إلى المجرة (الجزء الأول)- مهندس: نايف عثمان"
@@ -42,9 +42,8 @@
 					>Prof. Saleem Zaroubi - Cosmology and The Limits of Knowledge || البروفيسور سليم الزروبي - علوم الكون وحدود المعرفة</option>
 				</select>
 			</li>
-
 			<li class="list-group-item list-group-item-dark p-1 pb-0 m-1">
-				<select class="bg-transparent border-0 w-100 h5" v-model="p2D1" required>
+				<select class="bg-transparent border-0 w-100 h5" v-model="p2D1">
 					<option value disabled>Select p2D1</option>
 					<option
 						value="Eng. Yasser Abu-Elhassab - Star's Fiction....Space in Science Fiction || م. ياسر أبو الحسب - خيال النجوم .... الفضاء في الخيال العلمي"
@@ -55,6 +54,34 @@
 					<option
 						value="Eng. Akram Amin Abdellatif - The Utilization of The New Space Race in Understanding Our Planet || م. أكرم أمين عبد اللطيف - الاستفادة من سباق الفضاء الجديد في فهم كوكبنا"
 					>Eng. Akram Amin Abdellatif - The Utilization of The New Space Race in Understanding Our Planet || م. أكرم أمين عبد اللطيف - الاستفادة من سباق الفضاء الجديد في فهم كوكبنا</option>
+				</select>
+			</li>
+			<li class="list-group-item list-group-item-dark p-1 pb-0 m-1">
+				<select class="bg-transparent border-0 w-100 h5" v-model="p1D2">
+					<option value disabled>Select p1D2</option>
+					<option
+						value="Dr. Youssef Moulane - Small Bodies: Source of Danger or Origin of Life? || د. يوسف مولان - الأجسام الصغيرة .. مصدر خطر أم أصل الحياة؟"
+					>Dr. Youssef Moulane - Small Bodies: Source of Danger or Origin of Life? || د. يوسف مولان - الأجسام الصغيرة .. مصدر خطر أم أصل الحياة؟"</option>
+					<option
+						value="Eng. Naief Osman - Secrets and Mysteries of the Universe from the Atom to The Galaxy (Part II) || م. نايف عثمان - أسرار الكون من الذرة إلى المجرة (الجزء الثاني)"
+					>Eng. Naief Osman - Secrets and Mysteries of the Universe from the Atom to The Galaxy (Part II) || م. نايف عثمان - أسرار الكون من الذرة إلى المجرة (الجزء الثاني)"</option>
+					<option
+						value="Ms. Mayssa El Yazidi - BepiColombo Space Mission and Mercury Exploration || السيدة ميساء اليزيدي - BepiColombo المهمة الفضائية و استكشاف عطارد"
+					>Ms. Mayssa El Yazidi - BepiColombo Space Mission and Mercury Exploration || السيدة ميساء اليزيدي - BepiColombo المهمة الفضائية و استكشاف عطارد"</option>
+				</select>
+			</li>
+			<li class="list-group-item list-group-item-dark p-1 pb-0 m-1">
+				<select class="bg-transparent border-0 w-100 h5" v-model="p2D2">
+					<option value disabled>Select p1D2</option>
+					<option
+						value="Dr. Muhammad Al-Asiri - What are space technologies' pros and cons? || تقنيات الفضاء ما لها وما عليها - د. محمد العصيري"
+					>Dr. Muhammad Al-Asiri - What are space technologies' pros and cons? || تقنيات الفضاء ما لها وما عليها - د. محمد العصيري"</option>
+					<option
+						value="Eng. Akram Amin Abdellatif - The Upcoming Challenges For Aerospace Engineering || م. أكرم أمين عبد اللطيف - التحديات القادمة لهندسة الطيران والفضاء"
+					>Eng. Akram Amin Abdellatif - The Upcoming Challenges For Aerospace Engineering || م. أكرم أمين عبد اللطيف - التحديات القادمة لهندسة الطيران والفضاء"</option>
+					<option
+						value="Ms.Shelli Brunswick - First Steps Toward Access and Opportunity in the Global Space Ecosystem || السيدة شيلي برونزويك - الخطوات الأولى نحو الوصول والفرص في نظام الفضاء العالمي"
+					>Ms.Shelli Brunswick - First Steps Toward Access and Opportunity in the Global Space Ecosystem || السيدة شيلي برونزويك - الخطوات الأولى نحو الوصول والفرص في نظام الفضاء العالمي"</option>
 				</select>
 			</li>
 		</ul>
@@ -77,10 +104,10 @@ export default {
 			email: "",
 			number: "",
 			arrived: false,
-			p1D1: "Didnt register For a Workshop",
-			p2D1: "Didnt register For a Workshop",
-			p1D2: "Didnt register For a Workshop",
-			p2D2: "Didnt register For a Workshop",
+			p1D1: "",
+			p2D1: "",
+			p1D2: "",
+			p2D2: "",
 		})
 		const submitVisitor = () => {
 			store.dispatch("addVisitor", {
@@ -88,10 +115,10 @@ export default {
 				email: visitorState.email,
 				number: visitorState.number,
 				arrived: visitorState.arrived,
-				p1D1: visitorState.p1D1,
-				p2D1: visitorState.p2D1,
-				p1D2: visitorState.p1D2,
-				p2D2: visitorState.p2D2,
+				p1D1: visitorState.p1D1 || "Didnt register For a Workshop",
+				p2D1: visitorState.p2D1 || "Didnt register For a Workshop",
+				p1D2: visitorState.p1D2 || "Didnt register For a Workshop",
+				p2D2: visitorState.p2D2 || "Didnt register For a Workshop",
 			})
 			cancelForm()
 		}
